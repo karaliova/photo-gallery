2e09ca5506385cafe636c06c85ca144eeaaee594
karaliova <krudoch@gmail.com>
2024-09-12 18:49:03 +0200

    12_09_2024_finalrefactortest

--- app.py
+++ app.py
@@ -1,88 +1,58 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, redirect, url_for, flash, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import RegistrationForm, LoginForm
-from db import db
+from forms import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///photo_stock.db'
+app.config['UPLOAD_FOLDER'] = 'static/images'
 
-# Initialize db and login_manager
-db.init_app(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-# Import models after initializing db
-from models import User, Photo
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    # Add default photos if the table is empty
-    if not Photo.query.first():
-        default_photos = [
-            {"image": "default1.jpg", "likes": 0},
-            {"image": "default2.jpg", "likes": 0},
-            {"image": "default3.jpg", "likes": 0}
-        ]
-        for photo in default_photos:
-            db.session.add(Photo(**photo))
-        db.session.commit()
+# In-memory data
+users = {
+    'user1': generate_password_hash('password1')
+}
+photos = [
+    {'id': 1, 'image': 'default1.jpg', 'likes': 0},
+    {'id': 2, 'image': 'default2.jpg', 'likes': 0},
+    {'id': 3, 'image': 'default3.jpg', 'likes': 0}
+]
 
 @app.route('/')
-@login_required
 def home():
-    photos = Photo.query.all()
-    return render_template('home.html', photos=photos)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Account created! You can now log in.', 'success')
+    if 'username' not in session:
         return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    
+    return render_template('home.html', photos=photos)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password, form.password.data):
-            login_user(user)
+        username = form.username.data
+        password = form.password.data
+        hashed_password = users.get(username)
+        if hashed_password and check_password_hash(hashed_password, password):
+            session['username'] = username
             flash('Login successful!', 'success')
             return redirect(url_for('home'))
         flash('Invalid credentials. Please try again.', 'danger')
     return render_template('login.html', form=form)
 
 @app.route('/logout')
-@login_required
 def logout():
-    logout_user()
+    session.pop('username', None)
     return redirect(url_for('home'))
 
 @app.route('/photo/<int:photo_id>', methods=['POST'])
-@login_required
 def photo_action(photo_id):
-    photo = Photo.query.get_or_404(photo_id)
-    
-    if 'like' in request.form:
-        photo.likes += 1
-        flash('Photo liked!', 'success')
-    elif 'buy' in request.form:
-        flash('Pretend you have bought this photo!', 'success')
-    
-    db.session.commit()
+    photo = next((p for p in photos if p['id'] == photo_id), None)
+    if photo:
+        if 'like' in request.form:
+            photo['likes'] += 1
+            flash('Photo liked!', 'success')
+        elif 'buy' in request.form:
+            flash('Pretend you have bought this photo!', 'success')
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
